ff5975c8822512b099b14c88af0baaf8ce94974e
Todd Johnson <grandamp@gmail.com>
2022-11-03 14:48:16 -0400

    Documentation: SpringFox or SpringDocs?

--- src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
+++ src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
@@ -8,14 +8,19 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/*
+ * SpringFox Docs:  https://springfox.github.io/springfox/docs/current/
+ *
+ * TODO:  See if we should use SpringDoc instead:  https://springdoc.org/migrating-from-springfox.html
+ */
 @Configuration
-public class ConfigurationSwagger {                                    
+public class ConfigurationSwagger {
     @Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.any())
+          .build();
     }
 }
