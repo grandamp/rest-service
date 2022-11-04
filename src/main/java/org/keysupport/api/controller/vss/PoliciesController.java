1b931c77c3fbeb5d4b9bedce2036f13eca50559d
Todd Johnson <grandamp@gmail.com>
2022-11-04 14:04:22 -0400

    Removed un-used loggers

--- src/main/java/org/keysupport/api/controller/vss/PoliciesController.java
+++ src/main/java/org/keysupport/api/controller/vss/PoliciesController.java
@@ -2,8 +2,6 @@ package org.keysupport.api.controller.vss;
 
 import org.keysupport.api.config.ConfigurationPolicies;
 import org.keysupport.api.pojo.vss.ValidationPolicies;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PoliciesController {
-
-	private final static Logger LOG = LoggerFactory.getLogger(PoliciesController.class);
 
 	@GetMapping(path = "/vss/v2/policies", produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ValidationPolicies> policies() {
