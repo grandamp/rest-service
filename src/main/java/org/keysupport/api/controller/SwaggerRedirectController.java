c47ce228d894902eed72ee64d9e5ec3728c51f66
Todd Johnson <grandamp@gmail.com>
2022-11-05 23:20:30 -0400

    Working on model changes (Note: Phillies just lost the World Series... Bummer)

--- src/main/java/org/keysupport/api/controller/SwaggerRedirectController.java
+++ src/main/java/org/keysupport/api/controller/SwaggerRedirectController.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class SwaggerRedirectController {
 
+	@Operation(hidden = true)
 	@RequestMapping("/")
     public RedirectView handleRoot() {
         return new RedirectView("https://api.keysupport.org/swagger-ui/index.html");
