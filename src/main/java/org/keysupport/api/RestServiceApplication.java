2d5d5177fbbeb41e27f5ea3d345891c062e52442
Todd Johnson <grandamp@gmail.com>
2022-11-22 12:07:32 -0500

    Updating documentation

--- src/main/java/org/keysupport/api/RestServiceApplication.java
+++ src/main/java/org/keysupport/api/RestServiceApplication.java
@@ -15,16 +15,11 @@ public class RestServiceApplication {
 
 	/*
 	 * TODO:  Define Spring Boot Properties:  https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
-	 *
-	 *   - AWS Elasic Beanstalk expects the server is running on port 5000 -vs- 8080
-	 *     - server.port=5000
-	 *
-	 *   - For testing, our base URL may not be known until the associated environment is establshed
-	 *     - Base URL Hard Coded in a few classes as:  http://api-keysupport-rest-dev.us-east-1.elasticbeanstalk.com/
 	 */
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(RestServiceApplication.class);
         app.setBannerMode(Banner.Mode.OFF);
+        LOG.info("Service Starting");
         app.run(args);
 
         /*
