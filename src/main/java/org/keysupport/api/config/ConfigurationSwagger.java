6ed6a5b2467455b430cc70a7f2d1d5be24994255
Todd Johnson <grandamp@gmail.com>
2022-11-26 01:05:16 -0500

    Deployment doc update

--- src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
+++ src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
@@ -23,6 +23,6 @@ public class ConfigurationSwagger {
 								.url("https://app.ens.domains/name/cryptomeanscryptography.eth/details"))
 						.license(new License().name("CC0 1.0 Universal")
 								.url("https://creativecommons.org/publicdomain/zero/1.0/"))
-						.version("https://github.com/grandamp/rest-service/tree/main"));
+						.version("https://github.com/grandamp/rest-service/tree/cache"));
 	}
 }
