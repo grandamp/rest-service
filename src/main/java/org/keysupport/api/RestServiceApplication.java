9f164a114d20c703b160777200f11396fe4cbe1e
Todd Johnson <grandamp@users.noreply.github.com>
2023-04-11 15:09:34 -0400

    Policies (#263)
    * Remove AWS CRT Client property
    
    * Adding ability to exclude/ignore intermediates from hint CMS files
    
    * Fetch policies first, so we know where to fetch intermediate hints from

--- src/main/java/org/keysupport/api/RestServiceApplication.java
+++ src/main/java/org/keysupport/api/RestServiceApplication.java
@@ -15,11 +15,6 @@ public class RestServiceApplication {
 	 * TODO:  Define Spring Boot Properties:  https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html
 	 */
     public static void main(String[] args) {
-    	/*
-    	 *  Set the default AWS Java2 Toolkit asynchronous HTTP client to AwsCrtAsyncHttpClient
-    	 */
-    	System.setProperty("software.amazon.awssdk.http.async.service.impl",
-    	"software.amazon.awssdk.http.crt.AwsCrtSdkHttpService");
         SpringApplication app = new SpringApplication(RestServiceApplication.class);
         LOG.info("Service Starting");
         app.run(args);
