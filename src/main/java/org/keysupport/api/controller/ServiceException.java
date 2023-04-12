f8c3a8ef6b43c4f92a43f865c3e6c5cb6b942f51
Todd Johnson <grandamp@users.noreply.github.com>
2023-04-12 14:49:24 -0400

    Adding ability to incorporate more than one TrustAnchor for complex validation policies (#274)

--- src/main/java/org/keysupport/api/controller/ServiceException.java
+++ src/main/java/org/keysupport/api/controller/ServiceException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class ServiceException extends RuntimeException {
 
+	
 	private static final long serialVersionUID = 1L;
 
 	public ServiceException(String message, Throwable cause) {
