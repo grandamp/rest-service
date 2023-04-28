d8fcdd1e75dca288119c28ff905da68129944ae6
Todd Johnson <grandamp@gmail.com>
2023-04-27 22:41:54 -0400

    Updating Contact URL

--- src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
+++ src/main/java/org/keysupport/api/config/ConfigurationSwagger.java
@@ -24,7 +24,7 @@ public class ConfigurationSwagger {
 				.info(new Info().title("Certificate Validation Service API")
 						.description("Current main branch of Spring Boot reference implementation for the Certificate Validation Service API")
 						.contact(new Contact().name("Todd E. Johnson").email("grandamp@gmail.com")
-								.url("https://app.ens.domains/name/cryptomeanscryptography.eth/details"))
+								.url("https://app.ens.domains/name/tej.eth/details"))
 						.license(new License().name("CC0 1.0 Universal")
 								.url("https://creativecommons.org/publicdomain/zero/1.0/"))
 						.version("https://github.com/grandamp/rest-service/"));
