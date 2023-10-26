b5ddae47b77d07304d1863839e5f31963c646895
Todd Johnson <grandamp@users.noreply.github.com>
2023-10-26 01:16:34 -0400

    Cleaning up v1 example (#463)
    * Cleaning up v1 example
    
    * Cleaning up v1 example logging

--- src/main/java/org/keysupport/api/pojo/vss/v1/V1ClientRequestHelper.java
+++ src/main/java/org/keysupport/api/pojo/vss/v1/V1ClientRequestHelper.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
-import org.keysupport.api.pkix.ValidatePKIX;
 import org.keysupport.api.pkix.X509Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class V1ClientRequestHelper {
 
-	private final static Logger LOG = LoggerFactory.getLogger(ValidatePKIX.class);
+	private final static Logger LOG = LoggerFactory.getLogger(V1ClientRequestHelper.class);
 
 	/**
 	 * POST /vss/rest/
@@ -25,7 +24,7 @@ public class V1ClientRequestHelper {
 	 * @param valPol
 	 * @param wantBackList
 	 */
-	public static V1VSSRequest validateCert(String vssHost, X509Certificate cert, String valPol,
+	public static V1VSSRequest validateCert(X509Certificate cert, String valPol,
 			List<V1WantBackTypeToken> wantBackList) {
 
 		byte[] certBytes = null;
@@ -34,8 +33,8 @@ public class V1ClientRequestHelper {
 		} catch (CertificateEncodingException e) {
 			e.printStackTrace();
 		}
-		org.keysupport.api.pojo.vss.v1.V1X509Certificate certList[] = new org.keysupport.api.pojo.vss.v1.V1X509Certificate[1];
-		org.keysupport.api.pojo.vss.v1.V1X509Certificate jsonCert = new org.keysupport.api.pojo.vss.v1.V1X509Certificate();
+		V1X509Certificate certList[] = new V1X509Certificate[1];
+		V1X509Certificate jsonCert = new V1X509Certificate();
 		jsonCert.x509Certificate = Base64.getEncoder().encodeToString(certBytes);
 		jsonCert.setAdditionalProperty("vssCertId", X509Util.getVssCertId(cert));
 		certList[0] = jsonCert;
