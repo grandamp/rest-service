a4665d153cef49d3c89bb038a4c6351df0df6c9f
Todd Johnson <grandamp@gmail.com>
2025-11-23 11:54:43 -0500

    Updates SecurityAutoConfiguration exclusion due to Spring Boot 4 package restructure

--- src/main/java/org/keysupport/api/RestServiceApplication.java
+++ src/main/java/org/keysupport/api/RestServiceApplication.java
@@ -1,7 +1,7 @@
 package org.keysupport.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RestServiceApplication {
